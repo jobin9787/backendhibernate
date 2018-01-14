adca648d021edb6fce273528b8d918657e1b3c70
Andre Jobin <jobin9787@yahoo.fr>
2018-01-13 22:57:02 -0500

    first commit

--- src/main/java/ca/paruvendu/repository/GiftRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.paruvendu.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ca.paruvendu.domain.Gift;
-
-public interface GiftRepository extends CrudRepository<Gift,Long>  {
-
-}
--- src/main/java/ca/paruvendu/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.paruvendu.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ca.paruvendu.domain.security.Role;
-
-public interface RoleRepository extends CrudRepository<Role,Long>{
-
-}
