731987447d4fb5f95f2ea86b9b9d31ddf868523e
Andre Jobin <jobin9787@yahoo.fr>
2018-12-04 10:40:38 -0500

    remot host

--- src/main/java/ca/paruvendu/config/RequestFilter.java
+++ src/main/java/ca/paruvendu/config/RequestFilter.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,14 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class RequestFilter implements Filter{
 	
+	private static final Logger logger = LoggerFactory.getLogger(RequestFilter.class);
+	
 	public void doFilter(ServletRequest req,ServletResponse resp, FilterChain chain){
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse) resp;
+		logger.info("remote host "+ req.getRemoteHost());
+		logger.info("remote port "+ req.getRemotePort());
+		logger.info("remote addr "+ req.getRemoteAddr());
 		
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
