f2a1f79ccd292dd76a7f21b3153bd9fc0908cbdc
Andre Jobin <jobin9787@yahoo.fr>
2018-12-04 20:42:56 -0500

    remot host

--- src/main/java/ca/paruvendu/config/SecurityConfig.java
+++ src/main/java/ca/paruvendu/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().httpBasic().and().authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().
+		antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 	}
 	
 	@Autowired
