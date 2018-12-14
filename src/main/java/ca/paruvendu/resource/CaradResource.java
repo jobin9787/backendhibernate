6330c486433f4d1620f98cc6666c8d9b6f8844e2
Andre Jobin <jobin9787@yahoo.fr>
2018-12-13 19:18:03 -0500

    cross origin

--- src/main/java/ca/paruvendu/resource/CaradResource.java
+++ src/main/java/ca/paruvendu/resource/CaradResource.java
@@ -67,7 +67,7 @@ public class CaradResource {
 			
 			int i=1;
 			logger.info("create file");			
-			File theDir = new File("/var/www/upload/image/carad/"+id);
+			File theDir = new File("/var/www/image/carad/"+id);
 			Path path = Paths.get(theDir.toString());
 			String fullpath=path.toUri().toString();
 			logger.info("fullpath  "+fullpath);	
