bc69a14815f90cb6882c4eef67f76284d0814231
Andre Jobin <jobin9787@yahoo.fr>
2018-12-14 13:38:19 -0500

    orgin

--- src/main/java/ca/paruvendu/config/RequestFilter.java
+++ src/main/java/ca/paruvendu/config/RequestFilter.java
@@ -29,7 +29,8 @@ public class RequestFilter implements Filter{
 		logger.info("remote Origin "+ request.getHeader("Origin"));
 		
 
-		response.addHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
+		//response.addHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token");
 		response.setHeader("Access-Control-Max-Age", "3600");
