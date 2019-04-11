395bb35e39968f8fd5180b4ce77f89edeff7be9c
Andre Jobin <jobin9787@yahoo.fr>
2019-04-11 11:50:06 -0400

    remove security

--- src/main/java/ca/paruvendu/ParuvenduAngularApplication.java
+++ src/main/java/ca/paruvendu/ParuvenduAngularApplication.java
@@ -16,9 +16,9 @@ import ca.paruvendu.domain.security.UserRole;
 import ca.paruvendu.service.UserService;
 
 @SpringBootApplication
-public class ParuvenduAngularApplication extends SpringBootServletInitializer  implements CommandLineRunner {
+public class ParuvenduAngularApplication extends SpringBootServletInitializer   {
 
-
+//	implements CommandLineRunner
 
 	
 	@Autowired 
@@ -34,39 +34,39 @@ public class ParuvenduAngularApplication extends SpringBootServletInitializer  i
 
 	
 	
-	@Override 
-	public void run(String... args){
-		
-		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Doe");
-		user1.setEmail("testtest@test.ca");
-		user1.setUsername("j1");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		Set<UserRole> userRole = new HashSet<>();
-		Role role1 = new Role();
-		role1.setName("USER_ROLE");
-		role1.setRoleId(1);
-		userRole.add(new UserRole(user1,role1));
-		
-		userService.createUser(user1,userRole);
-		
-		userRole.clear();
-		User user2 = new User();
-		user2.setFirstName("Admin");
-		user2.setLastName("Doe");
-		user2.setEmail("admin@test.ca");
-		user2.setUsername("test1@tst.ca");
-		user2.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		
-		Role role2 = new Role();
-		role2.setName("ADMIN_ROLE");
-		role2.setRoleId(2);
-		userRole.add(new UserRole(user2,role2));
-
-		userService.createUser(user2,userRole);
-		
-		userRole.clear();
-		
-	}
+//	@Override 
+//	public void run(String... args){
+//		
+//		User user1 = new User();
+//		user1.setFirstName("John");
+//		user1.setLastName("Doe");
+//		user1.setEmail("testtest@test.ca");
+//		user1.setUsername("j1");
+//		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+//		Set<UserRole> userRole = new HashSet<>();
+//		Role role1 = new Role();
+//		role1.setName("USER_ROLE");
+//		role1.setRoleId(1);
+//		userRole.add(new UserRole(user1,role1));
+//		
+//		userService.createUser(user1,userRole);
+//		
+//		userRole.clear();
+//		User user2 = new User();
+//		user2.setFirstName("Admin");
+//		user2.setLastName("Doe");
+//		user2.setEmail("admin@test.ca");
+//		user2.setUsername("test1@tst.ca");
+//		user2.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+//		
+//		Role role2 = new Role();
+//		role2.setName("ADMIN_ROLE");
+//		role2.setRoleId(2);
+//		userRole.add(new UserRole(user2,role2));
+//
+//		userService.createUser(user2,userRole);
+//		
+//		userRole.clear();
+//		
+//	}
 }
